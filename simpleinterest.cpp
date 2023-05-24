afa3110ab264ef87469070ada3bcc21fb38aaeac
Vidit Chaudhary <132295390+vidit58@users.noreply.github.com>
2023-05-25 01:25:19 +0530

    Create simpleinterest.cpp

---
+++ simpleinterest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float p, r, t, si, ci;
+  
+  cout << "Enter principal amount: ";
+  cin >> p;
+ 
+  cout << "Enter rate of interest: ";
+  cin >> r;
+  
+  cout << "Enter time period: ";
+  cin >> t;
+  
+  // Calculate simple interest 
+  si = (p * r * t) / 100;   
+  
+  // Calculate compound interest
+  ci = p * (pow((1 + r/100), t) - 1);  
+  
+  cout << "Simple interest = " << si << endl;
+  cout << "Compound interest = " << ci;  
+} 
